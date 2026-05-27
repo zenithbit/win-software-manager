373da4ae00f6a58b8be300fd3ff3447f52a7c2d7
zenithbit <hongphuc.tnpt@gmail.com>
2026-05-27 13:45:10 +0700

    make some fixes

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import SoftwareManager from "@/app/components/SoftwareManager";
 import { getAll } from "@/lib/db";
 import { getCategories } from "@/lib/categoriesDb";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const software = await getAll();
   const categories = await getCategories();
